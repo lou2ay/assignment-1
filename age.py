4b109c92ac91bc7f21d22a49a11f7e99b2847133
louay <johndoe@example.com>
2025-02-12 23:13:43 +0200

    create <> age

---
+++ age.py
@@ -0,0 +1,10 @@
+# EX1
+age = int(input("enter your age: "))
+if age < 10:
+    print("price is 10$" )
+elif 10 <= age >18:
+    print ("price is 15$" )
+elif 18<= age > 70:
+    print("price is 20$" )
+else:
+    print("price is 25$ ")
