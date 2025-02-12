e955c2a2abe6607855b73917ea102d2196fd267d
louay <johndoe@example.com>
2025-02-13 00:04:05 +0200

    create <> login process

---
+++ login.py
@@ -0,0 +1,7 @@
+# Ex 3
+username = input ("enter username")
+password = input ("enter password")
+if username == "admin" and password == "1234":
+    print("access granted ")
+else:
+    print("access denied")
